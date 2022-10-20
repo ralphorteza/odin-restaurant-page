03822542bf79c3713c95ed29758fb2f9f445ed5b
ralph orteza <ralphlorteza@gmail.com>
2022-10-20 14:40:54 -0700

    Replace header and image code blocks to factory functions

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,22 +1,34 @@
 import backgroundImg from '../assets/images/background-img.jpg'
 
 
-const home = function() {
-  const content = document.getElementById('content');
-  
+
+const headerFactory = () => {
   const header = document.createElement('div');
   header.classList.add("header");
   header.textContent = "Here is the header!";
-  content.appendChild(header);
+  
+  return header;
+};
 
+const backGroundImg = () => {
   const imgContainer = document.createElement('div');
   imgContainer.classList.add('image-container');
   const bgImage = document.createElement('img');
   bgImage.src = backgroundImg;
   imgContainer.appendChild(bgImage);
+  
+  return imgContainer;
+};
+
+const home = function() {
+  const content = document.getElementById('content');
+  
+  const header = headerFactory();
+  content.appendChild(header);
+
+  const imgContainer = backGroundImg();
   content.appendChild(imgContainer);
-
-
 }
+
 
 export default home;
